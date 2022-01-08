6529f1ed9df1dcbcd0e5f9889214ef91c853210e
stas.r <remnev.stas@gmail.com>
2022-01-08 10:48:33 +0300

    fix: text input && add logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,105 @@
 import './index.css';
+
+const container = document.querySelector('.container');
+const noTaskElement = container.querySelector('.no-task__text');
+const form = container.querySelector('.input');
+const addTaskButton = form.querySelector('.input__btn_action_add');
+// const addTaskButton = document.querySelector('.input__btn_action_add');
+const redContainer = container.querySelector('.task-container');
+
+const tasks = document.querySelectorAll('.task');
+const taskArr = Array.from(tasks);
+console.log(tasks.length);
+
+function renderHasTask() {
+  if (arr.length > 0) {
+    noTaskElement.classList.add('no-task_hidden');
+  }
+  return;
+}
+function renderNoTask() {
+  if (arr.length === 0) {
+    noTaskElement.classList.remove('no-task_hidden');
+  }
+}
+//функция добавления задачи из темплейта
+function addTask(taskValue) {
+  const taskTemplate = document.querySelector('#task-template').content;
+  const taskElement = taskTemplate.querySelector('.task').cloneNode(true);
+  taskElement.querySelector('.task__text').textContent = taskValue;
+
+  taskElement
+    .querySelector('.task__remove')
+    .addEventListener('click', (evt) => {
+      arr.pop(valInp);
+      evt.target.closest('.task').remove();
+      renderNoTask();
+    });
+  redContainer.append(taskElement);
+}
+
+let arr = [];
+const task = document.querySelector('.input__text_type_task');
+let valInp = task.value;
+
+//Добавление таски
+// addTaskButton.addEventListener('click', () => {
+//   if (task.value.length > 0) {
+//     console.log('work');
+//     addTask(task.value);
+//     arr.push(valInp);
+//     task.value = '';
+//     setSubmitButtonState();
+//   } else {
+//     console.log('yop');
+//   }
+//   renderHasTask();
+// });
+
+addTaskButton.addEventListener('click', () => {
+  pushTask();
+});
+function pushTask() {
+  if (task.value.length > 0) {
+    console.log('work');
+    addTask(task.value);
+    arr.push(valInp);
+    task.value = '';
+    setSubmitButtonState();
+  } else {
+    console.log('yop');
+  }
+  renderHasTask();
+}
+
+form.addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 13) {
+    console.log('enter');
+    // addTask();
+    pushTask();
+  } else {
+  }
+
+  // renderHasTask();
+});
+
+// form.addEventListener('keypress', (keyPressed) => {
+//   const keyEnter = 13;
+//   if (keyPressed.which == keyEnter) {
+//     console.log('enter');
+//   }
+// });
+function setSubmitButtonState(isFormValid) {
+  if (isFormValid) {
+    addTaskButton.removeAttribute('disabled');
+    addTaskButton.classList.remove('input__btn_disabled');
+  } else {
+    addTaskButton.setAttribute('disabled', true);
+    addTaskButton.classList.add('input__btn_disabled');
+  }
+}
+
+form.addEventListener('input', function (evt) {
+  const isValid = task.value.length > 0;
+  setSubmitButtonState(isValid);
+});
